792c377f9a4467947ca1c0d472f3ec1e77051d4d
SC Graphics <scgraphics@SCs-iMac.local>
2018-12-21 15:14:51 -0500

    add testing

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards
+  """
   def create_deck do
     values = [
       "Ace",
@@ -27,10 +34,30 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Checks to see if the deck contains a given `card`
+
+    ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand
+
+    ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Clubs"]
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
